d0f8c2f23396600cfa7549259bb35fa3de1e43ea
Inna Subbotina <inness-01@yandex.ru>
2023-11-23 13:41:59 +0300

    Add new classes

---
+++ app/src/main/java/com/example/cafeorder/NewActivity.java
@@ -0,0 +1,13 @@
+package com.example.cafeorder;
+
+import android.view.View;
+import android.widget.RadioButton;
+
+public class NewActivity {
+
+    public void onClickChangeDrink(View view) {
+        RadioButton button = (RadioButton) view;
+        int id = button.getId();
+
+    }
+}
